c5f68eb963566d5ce21ae25ceeefaceb19aba761
Alibaba2023 <126192888+Alibaba2023@users.noreply.github.com>
2023-09-07 19:18:27 +0430

    remove the fizzbuzz

--- solver.rb
+++ solver.rb
@@ -11,15 +11,4 @@ class Solver
     word.reverse
   end
 
-  def fizzbuzz(number)
-    if (number % 3).zero? && (number % 5).zero?
-      'fizzbuzz'
-    elsif (number % 3).zero?
-      'fizz'
-    elsif (number % 5).zero?
-      'buzz'
-    else
-      number.to_s
-    end
-  end
 end
