b63897e92f02cac569941ff8746ae55c285c8134
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-09-07 09:43:16 -0500

    Add unit tests for reverse and fizzbuzz methods

--- solver_spec.rb
+++ solver_spec.rb
@@ -27,5 +27,36 @@ RSpec.describe Solver do
     end
   end
 
+  describe '#reverse' do
+    it 'returns the reversed string' do
+      expect(solver.reverse('hello')).to eq('olleh')
+      expect(solver.reverse('world')).to eq('dlrow')
+    end
+
+    it 'returns an empty string for an empty string' do
+      expect(solver.reverse('')).to eq('')
+    end
+  end
+
+  describe '#fizzbuzz' do
+    it 'returns "fizz" for multiples of 3' do
+      expect(solver.fizzbuzz(3)).to eq('fizz')
+      expect(solver.fizzbuzz(9)).to eq('fizz')
+    end
+
+    it 'returns "buzz" for multiples of 5' do
+      expect(solver.fizzbuzz(5)).to eq('buzz')
+      expect(solver.fizzbuzz(10)).to eq('buzz')
+    end
+
+    it 'returns "fizzbuzz" for multiples of both 3 and 5' do
+      expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+      expect(solver.fizzbuzz(30)).to eq('fizzbuzz')
+    end
+
+    it 'returns the number as a string for other cases' do
+      expect(solver.fizzbuzz(1)).to eq('1')
+      expect(solver.fizzbuzz(7)).to eq('7')
+    end
   end
 end
